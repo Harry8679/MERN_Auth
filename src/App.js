e12336228112674aab9f3da4485c54239060ec7a
Harry MacCode <emarh.harry.code@gmail.com>
2023-08-13 12:14:54 +0200

    Login Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Layout from './layout/Layout';
+import Login from './pages/auth/Login';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route element={<Layout><Home /></Layout>} path='/' />
+          <Route element={<Login />} path='/connexion' />
         </Routes>
       </Router>
     </>
---
+++ src/pages/auth/Login.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import styles from './auth.module.scss';
+import Card from '../../components/card/Card';
+import { BiLogIn } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleInputChange = () => {}
+
+  const loginUser = () => {};
+  return (
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+            <div className="--flex-center">
+                <BiLogIn size={35} color='#999' />
+            </div>
+            <h2>Connexion</h2>
+            <div className="--flex-center">
+                <button className="--btn --btn-google">Connexion avec Google</button>
+            </div>
+            <br />
+            <p className="--text-center --fw-bold">Ou</p>
+
+            <form onSubmit={loginUser}>
+                <input type="email" placeholder='Email' required name='email' value={email} onChange={handleInputChange} />
+                <input type="password" placeholder='Mot de passe' required name='password' value={password} onChange={handleInputChange} />
+
+                <button type='submit' className="--btn --btn-primary --btn-block">Connexion</button>
+            </form>
+            <Link to='/forgot'>Mot de passe oublié</Link>
+
+            <span className={styles.register}>
+                <Link to='/'>Accueil</Link>
+                <p> &nbsp;Pas encore de compte? &nbsp;</p>
+                <Link to='/register'>Inscription</Link>
+            </span>
+        </div>
+      </Card>
+    </div>
+  )
+}
+
+export default Login;
