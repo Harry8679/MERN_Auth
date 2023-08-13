2c425d472ec3992e2c8a24abee924e101fe0fa6c
Harry MacCode <emarh.harry.code@gmail.com>
2023-08-13 11:29:23 +0200

    Home Page created

---
+++ NOTES.md
@@ -0,0 +1,14 @@
+* Udemy Course : https://www.udemy.com/course/advanced-mern-stack-authentication/
+
+* Author : Ewomazino
+
+* MongoDB :
+    * email : emarh.harry.code@gmail.com
+    * projet : EbangMezuiShopIT
+
+* GitHub : 
+    * email: emarh.harry.code@gmail.com
+    * nom du projet : MERN_Auth
+
+* Deployment (API):
+    * 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,32 @@ import React from 'react'
 import './Home.scss';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
+import loginImage from '../../assets/login.svg';
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <h1>Hi From Home</h1>
+        <section className="container hero">
+            <div className="hero-text">
+                <h2>Meilleur Système de Connexion avec les technologies MERN</h2>
+                <p>
+                    Avec les technologies MERN (Mongo Express React Node) on peut faire des applications Web. Et dans ces applications web, 
+                    on peut faire de supers manières de faire des Authentifications et Autorisations.
+                </p>
+                <p>
+                    Pour cela, on va Implémenter des systèmes tels que l'Inscription pour un Utilisateur, ainsi que beaucoup d'autres choses telles que :
+                    la Connexion, la Modification du Mot de Passe, Connexion avec les Réseaux sociaux, Notifications par Email et des Permissions ...
+                </p>
+                <div className="hero-buttons --flex-start">
+                    <button className="--btn --btn-danger">Inscription</button>
+                    <button className="--btn --btn-primary">Connexion</button>
+                </div>
+            </div>
+            <div className="hero-image">
+                <img src={loginImage} alt="Authentification" />
+            </div>
+        </section>
       <Footer />
     </div>
   )
