0d7ed2c90dce67be9534a67acef0a52e074759c9
Harry MacCode <emarh.harry.code@gmail.com>
2023-08-15 00:54:23 +0200

    Password Input Component

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -3,6 +3,7 @@ import styles from './auth.module.scss';
 import Card from '../../components/card/Card';
 import { BiLogIn } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
+import PasswordInput from '../../components/passwordInput/PasswordInput';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -27,7 +28,8 @@ const Login = () => {
 
             <form onSubmit={loginUser}>
                 <input type="email" placeholder='Email' required name='email' value={email} onChange={handleInputChange} />
-                <input type="password" placeholder='Mot de passe' required name='password' value={password} onChange={handleInputChange} />
+                {/* <input type="password" placeholder='Mot de passe' required name='password' value={password} onChange={handleInputChange} /> */}
+                <PasswordInput placeholder='Mot de Passe' value={password} name='password' onChange={handleInputChange} />
 
                 <button type='submit' className="--btn --btn-primary --btn-block">Connexion</button>
             </form>
