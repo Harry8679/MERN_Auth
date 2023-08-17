9c27694539783c0744c1256c1bbe6f85dd1a2fbd
Harry MacCode <emarh.harry.code@gmail.com>
2023-08-17 10:54:50 +0200

    Loader Component

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Loader.scss';
+import ReactDOM from 'react-dom';
+import loadingImg from '../../assets/loader.gif';
+
+const Loader = () => {
+  return ReactDOM.createPortal(
+    <div className='wrapper'>
+      <div className='loader'>
+        <img src={loadingImg} alt='loading' />
+      </div>
+    </div>, document.getElementById('loader')
+  )
+}
+
+export const Spinner = () => {
+    return (
+        <div className='--center-all'>
+            <img src={loadingImg} alt='Loading...' />
+        </div>
+    );
+}
+
+export default Loader;
