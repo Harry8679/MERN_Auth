a22ff460cffb9422f2a32f02a285f4d1ec6ca903
Harry MacCode <emarh.harry.code@gmail.com>
2023-08-17 08:15:06 +0200

    User Table

--- src/pages/userList/UserList.js
+++ src/pages/userList/UserList.js
@@ -3,6 +3,7 @@ import './UserList.scss';
 import PageMenu from '../../components/pageMenu/PageMenu';
 import UserStats from '../../components/userStats/UserStats';
 import Search from '../../components/search/Search';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const UserList = () => {
   return (
@@ -12,9 +13,37 @@ const UserList = () => {
         <UserStats />
 
         <div className="user-list">
-            <div className="--flex-between">
-                <span><h3>Tous les utilisateurs</h3></span>
-                <span><Search /></span>
+            <div className="table">
+                <div className="--flex-between">
+                    <h3>Tous les utilisateurs</h3>
+                    <span><Search /></span>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>s/n</th>
+                            <th>Nom</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Changer de rôle</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>EBANG</td>
+                            <td>ebang.mezui@wizards.us</td>
+                            <td>Admin</td>
+                            <td>Changer de rôle</td>
+                            <td>
+                                <span>
+                                    <FaTrashAlt size={20} color='red' />
+                                </span>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
       </div>
