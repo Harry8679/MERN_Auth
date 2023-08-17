815f20b048842cabf70b74ce2c900c4c6c0222db
Harry MacCode <emarh.harry.code@gmail.com>
2023-08-17 08:02:56 +0200

    Search Component Added

--- src/pages/userList/UserList.js
+++ src/pages/userList/UserList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './UserList.scss';
 import PageMenu from '../../components/pageMenu/PageMenu';
 import UserStats from '../../components/userStats/UserStats';
+import Search from '../../components/search/Search';
 
 const UserList = () => {
   return (
@@ -9,6 +10,13 @@ const UserList = () => {
       <div className="container">
         <PageMenu />
         <UserStats />
+
+        <div className="user-list">
+            <div className="--flex-between">
+                <span><h3>Tous les utilisateurs</h3></span>
+                <span><Search /></span>
+            </div>
+        </div>
       </div>
     </section>
   )
